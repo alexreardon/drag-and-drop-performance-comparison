8f8aba904b4a349ecf22873b1a3bb3b4f8044da8
Alex Reardon <areardon@atlassian.com>
2022-08-31 15:17:49 +1000

    removing deferred entry point

--- pages/scenario/deferred-atlaskit-drag-and-drop.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-
-import Board from '../../pieces/scenario/deferred-atlaskit-drag-and-drop/board';
-
-const Baseline: NextPage = () => {
-  return <Board />;
-};
-
-export default Baseline;
