8da6e91b1ee23fdc2a96848957564cd1367b8db8
Declan Warn <dwarn@atlassian.com>
2022-09-27 09:57:31 +1000

    first pass on adding example a11y affordances

---
+++ pages/scenario/atlaskit-drag-and-drop-a11y.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+import Board from '../../pieces/scenario/atlaskit-drag-and-drop-a11y/board';
+
+const Baseline: NextPage = () => {
+  return <Board />;
+};
+
+export default Baseline;
