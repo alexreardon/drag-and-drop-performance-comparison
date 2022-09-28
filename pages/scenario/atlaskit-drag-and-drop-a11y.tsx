62f8317968c60404264a4028a34a9037ada902df
Declan Warn <dwarn@atlassian.com>
2022-09-28 13:21:50 +1000

    remove a11y scenario

--- pages/scenario/atlaskit-drag-and-drop-a11y.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-
-import Board from '../../pieces/scenario/atlaskit-drag-and-drop-a11y/board';
-
-const Baseline: NextPage = () => {
-  return <Board />;
-};
-
-export default Baseline;
