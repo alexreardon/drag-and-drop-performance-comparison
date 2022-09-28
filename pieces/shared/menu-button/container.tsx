82f3f5d44b5fc3c8c519b40c0c137a0efade961e
Declan Warn <dwarn@atlassian.com>
2022-09-28 14:31:15 +1000

    add comments to character match logic

--- pieces/shared/menu-button/container.tsx
+++ pieces/shared/menu-button/container.tsx
@@ -106,21 +106,41 @@ export const MenuButton = ({ label, children }: { label: string; children: React
         break;
     }
 
+    /**
+     * When pressing a letter (A-Z or a-z) then:
+     *
+     * - Moves focus to the next menu item with a label that starts with the typed character if such an menu item exists.
+     * - Otherwise, focus does not move.
+     */
     if (containerRef.current && /[a-z]/i.test(event.key)) {
       const letter = event.key.toLowerCase();
 
+      /**
+       * The menu items in the menu.
+       */
       const items = Array.from(containerRef.current.querySelectorAll('li'));
 
+      /**
+       * The index of the currently focused menu item.
+       */
       const activeIndex = items.indexOf(document.activeElement as HTMLLIElement);
 
-      let match = items
+      /**
+       * The next menu item that matches the typed character.
+       */
+      let match: HTMLElement | undefined;
+
+      // Initially search only the items after the currently selected one.
+      match = items
         .slice(activeIndex + 1)
         .find((item) => item.textContent?.toLowerCase().startsWith(letter));
 
+      // If there was no match found, wrap-around and search from the beginning.
       if (match === undefined) {
         match = items.find((item) => item.textContent?.toLowerCase().startsWith(letter));
       }
 
+      // Focus the match if one was found.
       if (match !== undefined) {
         match.focus();
       }
