d7a447cc9377104eeccca22f28f8ef8c8963729a
Declan Warn <dwarn@atlassian.com>
2022-09-29 15:06:31 +1000

    clean up container code

--- pieces/shared/menu-button/container.tsx
+++ pieces/shared/menu-button/container.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useCallback, useEffect, useReducer, useRef } from 'react';
+import { ReactNode, RefObject, useCallback, useEffect, useReducer, useRef } from 'react';
 
 import { css } from '@emotion/react';
 import { bind } from 'bind-event-listener';
@@ -31,7 +31,7 @@ type MenuButtonAction =
       shouldResetFocus: boolean;
     };
 
-function reducer(state: MenuButtonState, action: MenuButtonAction): MenuButtonState {
+function reducer(_: MenuButtonState, action: MenuButtonAction): MenuButtonState {
   if (action.type === 'open') {
     return {
       isOpen: true,
@@ -49,8 +49,38 @@ function reducer(state: MenuButtonState, action: MenuButtonAction): MenuButtonSt
   throw new Error('unreachable');
 }
 
+/**
+ * Closes the menu if there is a click outside of it.
+ */
+function useCloseOnOutsideClick(
+  ref: RefObject<HTMLUnknownElement>,
+  {
+    isOpen,
+    closeMenu,
+  }: { isOpen: boolean; closeMenu: ({ shouldResetFocus }: { shouldResetFocus: boolean }) => void },
+) {
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+
+    return bind(window, {
+      type: 'click',
+      listener: (event) => {
+        // Ignore clicks inside, we only care about _outside_ clicks.
+        if (ref.current?.contains(event.target as Node)) {
+          return;
+        }
+        closeMenu({ shouldResetFocus: true });
+      },
+    });
+  }, [closeMenu, isOpen, ref]);
+}
+
 export const MenuButton = ({ label, children }: { label: string; children: ReactNode }) => {
   const [state, dispatch] = useReducer(reducer, { isOpen: false, shouldResetFocus: true });
+
+  const ref = useRef<HTMLSpanElement>(null);
 
   const closeMenu = useCallback(({ shouldResetFocus }: { shouldResetFocus: boolean }) => {
     dispatch({ type: 'close', shouldResetFocus });
@@ -60,28 +90,10 @@ export const MenuButton = ({ label, children }: { label: string; children: React
     dispatch({ type: 'open', initialFocus });
   }, []);
 
-  const containerRef = useRef<HTMLSpanElement>(null);
-
-  // Close the menu on outside clicks
-  useEffect(() => {
-    if (!state.isOpen) {
-      return;
-    }
-
-    return bind(window, {
-      type: 'click',
-      listener: (event) => {
-        // Ignore clicks inside, we only care about _outside_ clicks.
-        if (containerRef.current?.contains(event.target as Node)) {
-          return;
-        }
-        closeMenu({ shouldResetFocus: true });
-      },
-    });
-  }, [closeMenu, state.isOpen]);
+  useCloseOnOutsideClick(ref, { isOpen: state.isOpen, closeMenu });
 
   return (
-    <span css={containerStyles} ref={containerRef}>
+    <span css={containerStyles} ref={ref}>
       <Trigger
         isOpen={state.isOpen}
         shouldResetFocus={state.isOpen ? false : state.shouldResetFocus}
