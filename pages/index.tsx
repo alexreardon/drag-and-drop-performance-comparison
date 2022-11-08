06635f754b467aa25e193138827fae77ebc5dc3d
Alex Reardon <areardon@atlassian.com>
2022-11-08 16:56:27 +1100

    fixing ally

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import type { GetStaticProps, NextPage } from 'next';
 import fs from 'fs/promises';
 import path from 'path';
 import Link from 'next/link';
+import githubIcon from '../pieces/shared/github.svg';
+import { css, Global } from '@emotion/react';
+import { token } from '@atlaskit/tokens';
 
 export const getStaticProps: GetStaticProps = async () => {
   const filePaths: string[] = await fs.readdir(path.join(process.cwd(), 'pages', 'scenario'));
@@ -13,26 +16,63 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
+const headerStyles = css({
+  background: `linear-gradient(43deg, ${token('color.accent.boldGreen')} 0%, ${token(
+    'color.accent.boldOrange',
+  )} 46%, ${token('color.accent.boldRed')} 100%)`,
+  '-webkit-background-clip': 'text',
+  '-webkit-text-fill-color': 'transparent',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  fontSize: '3rem',
+});
+const subHeaderStyles = css({
+  textTransform: 'uppercase',
+  fontSize: '1.2rem',
+  fontWeight: 'bold',
+});
+
+const stackStyles = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 'var(--grid)',
+});
+
+const repoLinkStyles = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: 'calc(var(--grid) / 2)',
+});
+
 const Home: NextPage = ({ slugs }: { slugs?: string[] }) => {
   return (
     <>
-      <h1>
-        <code>react.js</code> drag and drop libraries performance comparison
-      </h1>
-      <p>
-        Repo:
-        <a href="https://github.com/alexreardon/drag-and-drop-performance-comparison">
-          github.com/alexreardon/drag-and-drop-performance-comparison
-        </a>
-      </p>
-      <h2>Available scenarios:</h2>
-      <ul>
-        {slugs?.map((slug) => (
-          <li key={slug}>
-            <Link href={slug}>{slug}</Link>
-          </li>
-        ))}
-      </ul>
+      <div css={stackStyles}>
+        <h1 css={headerStyles}>
+          <span>Drag and drop libraries</span>
+          <span css={subHeaderStyles}>performance comparison</span>
+        </h1>
+        <p>
+          <a
+            css={repoLinkStyles}
+            href="https://github.com/alexreardon/drag-and-drop-performance-comparison"
+          >
+            <img {...githubIcon} alt="" draggable={false} />
+            Repo
+          </a>
+        </p>
+        <h2>Available scenarios</h2>
+        <ul>
+          {slugs?.map((slug) => (
+            <li key={slug}>
+              <Link href={slug}>{slug}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
